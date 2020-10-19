6084292e2325833030553c94de08f4da9d7e77a6
ivin-3-69 <ivin.kuriakose.2000@gmail.com>
2020-10-19 17:38:47 +0530

    task showing and adding

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,21 @@
-from django.http import HttpResponse
+from django.http import HttpResponse ,HttpResponseRedirect
 from django.shortcuts import render
+from django.utils import timezone
+from . import models
 
 def base(request):
-    return render(request, "todo/base.html")
+    task_objects = models.ToDoTasks.objects.all()
+    context = {
+        "tasks" : task_objects
+    }
+    print(task_objects)
+    #models.ToDoTasks.objects.all().delete()
+    return render(request, "todo/index.html" ,context)
+
+def add(request):
+    new_event = request.POST.get("new-task")
+    present = timezone.now()
+    models.ToDoTasks.objects.create(task_date = present ,task = new_event)
+    return HttpResponseRedirect("/todo/")
+    
+    
